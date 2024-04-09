b1232f78ea89e8c06fa8384f1e3e0eb8d8678f2c
JEFFREY-Bonson <jeffrey.bonson@ibm.com>
2024-04-09 18:50:48 +0530

    vue js layout changed

--- vue-bookstore/src/App.vue
+++ vue-bookstore/src/App.vue
@@ -1,74 +1,52 @@
+<script setup>
+  import { ref } from 'vue';
+  import { useBreakpoint } from 'vuestic-ui';
+
+  const showSidebar = ref(true);
+
+  const breakpoints = useBreakpoint();
+</script>
+
+
 <template>
-  <VaCard
-    color="background-element"
-    class="p-3"
+  <VaLayout 
+    style="height: 500px"
+    :left="{ absolute: breakpoints.smDown }"
   >
-    <div class="row">
-      <div class="flex flex-col md12">
-        <div class="item">
-          <NavBar />
-        </div>
-      </div>
-      <div class="flex flex-col md2">
-        <div class="item">
-          <SideBar/>
-        </div>
-      </div>
-      <div class="flex flex-col md10">
-        <div class="item">
-          <nav>
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link> |
-            <router-link to="/books">Books</router-link>
-          </nav>
-          <router-view/>
-        </div>
-      </div>
-    </div>
-    <br>
-    
-  </VaCard>
+    <template #top>
+      <VaNavbar color="primary" class="py-2">
+        <template #left>
+          <VaButton :icon="showSidebar ? 'menu_open' : 'menu'" @click="showSidebar = !showSidebar" />
+        </template>
+        <template #center>
+          <VaNavbarItem class="font-bold text-lg">
+            eShop
+          </VaNavbarItem>
+        </template>
+      </VaNavbar>
+    </template>
+
+    <template #left>
+      <VaSidebar v-model="showSidebar">
+        <SideBar />
+      </VaSidebar>
+    </template>
+
+    <template #content>
+      <main class="p-4">
+        <router-view/>
+      </main>
+    </template>
+  </VaLayout>
 </template>
 
-<style>
-@import "vuestic-ui/styles/grid";
-
-.item {
-  border: 1px solid var(--va-background-border);
-  background-color: var(--va-background-primary);
-  text-align: center;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
 <script>
 import SideBar from './components/SideBar.vue';
-import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'BookList',
   components: {
     SideBar,
-    NavBar,
   },
-}
+};
 </script>
